917837cd8ddd8be5369995bc2e58122fa777a041
Ratkovski <drezcry@hotmail.com>
2023-07-16 20:33:55 -0300

    chore: variables

---
+++ variables/variables.py
@@ -0,0 +1,35 @@
+x = 100
+print(type(x))
+x = 654 * 6734
+print(type(x))
+print(x)
+x = 3.14
+print(x)
+print(type(x))
+x = "Simplilearn"
+print(x)
+x = 'Simplilearn'
+print(x)
+print(type(x))
+x = [14, 67, 9]
+print(x)
+print(type(x))
+print(x[0])
+print(x[2])
+x[2] = 67
+print(x)
+x = (4, 8, 6)
+print(x)
+print(type(x))
+print(x[1])
+# x[1] = 5  # not supported
+# jupiter
+# x = open('variable_com.ipynb', 'r')
+# print(type(x))
+(x, y, z) = 5, 10, 7
+print(x)
+print(y)
+print(z)
+(x, y, z) = 1, 1, 1
+x = y = z = 1
+print(x, y, z)
